054d605bedbd3479ba327bf66f20248e7ec06de1
Bustanil Arifin <bustanil.arifin@grabtaxi.com>
2022-09-28 17:25:41 +0800

    [learn-scalajs] hello world

---
+++ learn_scalajs/src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -0,0 +1,16 @@
+package tutorial.webapp
+
+import org.scalajs.dom
+import org.scalajs.dom.document
+
+object TutorialApp {
+  def main(args: Array[String]): Unit = {
+    appendPar(document.body, "Hello Bustanil")
+  }
+
+  def appendPar(targetNode: dom.Node, text: String) : Unit = {
+    val parNode = document.createElement("p")
+    parNode.textContent = text
+    targetNode.appendChild(parNode)
+  }
+}
