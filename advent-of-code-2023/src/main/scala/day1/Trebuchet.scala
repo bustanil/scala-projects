1d645eeec883a4baeef0d7f4dd7cc95a84c18b8e
Bustanil Arifin <bustanil.arifin@grabtaxi.com>
2023-12-03 06:53:27 +0800

    aoc2023 day1 part2

--- advent-of-code-2023/src/main/scala/day1/Trebuchet.scala
+++ advent-of-code-2023/src/main/scala/day1/Trebuchet.scala
@@ -1,26 +1,34 @@
 package day1
 
 import scala.io.Source
+import scala.quoted.runtime.Patterns
+import scala.util.matching.Regex
 
 object Trebuchet {
 
-  def main(args: Array[String]): Unit = {
-    val result = Source.fromFile("/home/bustanil/aoc2023/day1/part1/input.txt", "utf-8")
+  def main(args: Array[String]): Unit = 
+    val result = Source.fromFile(args(0), "utf-8")
       .getLines()
       .map(calibrationValue)
       .sum
 
     println(s"Calibration value: $result")
-  }
 
   def calibrationValue(s: String): Int =
-    // collect all digits into a List of Int
-    val digits: List[Int] = 
-      s.foldLeft(List.empty)((acc, c) => if (c.isDigit) acc :+ c.asDigit else acc)
+    val digitPattern = "^(\\d{1}|one|two|three|four|five|six|seven|eight|nine)".r
+    val words = List("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+    
+    def collectDigit(s: String, digits: List[Int]): List[Int] = 
+      if (s.length() == 0) digits
+      else
+        digitPattern.findFirstIn(s) match
+          case Some(token) if token.length() == 1 =>
+            collectDigit(s.substring(1), digits :+ token.toInt)
+          case Some(token) => collectDigit(s.substring(1), digits :+ words.indexOf(token) + 1)
+          case None => collectDigit(s.substring(1), digits)
 
-    digits match {
-      case n :: Nil => n * 10 + n // match singleton List
+    collectDigit(s, List.empty) match 
+      case n :: Nil    => n * 10 + n // match singleton List
       case n +: _ :+ m => n * 10 + m // match first and last element
-      case _ => 0
-    }
+      case _           => 0
 }
