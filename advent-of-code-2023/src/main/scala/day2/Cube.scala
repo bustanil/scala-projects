e6300441ba1e3b8b866f5821d2917536111cddd4
Bustanil Arifin <bustanil@protonmail.com>
2023-12-04 02:02:38 +0800

    aoc 2023 day 2 part 2

--- advent-of-code-2023/src/main/scala/day2/Cube.scala
+++ advent-of-code-2023/src/main/scala/day2/Cube.scala
@@ -8,19 +8,52 @@ object Cube {
   type Game = List[Cubes] 
 
   def main(args: Array[String]): Unit = {
+    // part 1
     val config = Map(("red" -> 12), ("green" -> 13), ("blue" -> 14))
 
     val sum = Source.fromFile(args(0))
     .getLines()
     .map(parseLine)
     .map(maybeGame => maybeGame match
-      case Some(gameTuple) if isPossible(gameTuple(1), config) => gameTuple(0)
+      case Some((id, game)) if isPossible(game, config) => id
       case _ => 0 
     )
     .sum
 
     println(sum)
+
+    // part 2
+    val power = Source.fromFile(args(0))
+    .getLines()
+    .map(parseLine)
+    .map(maybeGame => maybeGame match
+      case Some((id, game)) => powerOfMinimumCubes(game)
+      case _ => 0 
+    )
+    .sum
+
+    println(power)
   }
+
+  extension (m: Cubes) {
+    def combineWith(key: String)(f: (Int, Int) => Int)(that: Cubes) =
+      m.updatedWith(key)(valueOpt => 
+        (valueOpt, that.get(key)) match
+          case (None, _) => None
+          case (Some(value), None) => Some(value)
+          case (Some(a), Some(b)) => Some(f(a, b))
+        )
+  }
+
+  def powerOfMinimumCubes(game: Game): Int =
+    val init = Map(("red" -> 0), ("blue" -> 0), ("green" -> 0))
+    game.foldLeft(init)((acc, cubes) => {
+      acc.combineWith("red")(Math.max)(cubes)
+        .combineWith("blue")(Math.max)(cubes)
+        .combineWith("green")(Math.max)(cubes)
+    })
+    .values
+    .reduce((a, b) => a * b)
 
   def parseLine(s: String): Option[(Int, Game)] =
     val gamePattern = "Game (\\d+): (.+)".r
