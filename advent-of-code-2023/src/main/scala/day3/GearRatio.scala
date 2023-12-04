1f3a165b38bdca98e032476db3ea63ff5d01cb47
Bustanil Arifin <bustanil.arifin@grabtaxi.com>
2023-12-05 07:41:06 +0800

    cleanup

--- advent-of-code-2023/src/main/scala/day3/GearRatio.scala
+++ advent-of-code-2023/src/main/scala/day3/GearRatio.scala
@@ -4,9 +4,8 @@ import scala.io.Source
 
 object GearRatio {
 
-  type Number = Int
-  type SymbolMap = Map[Int, List[Int]]
-  type NumberMap = Map[Int, List[(Number, Int, Int)]]
+  type SymbolMap = Map[Int, List[Int]]  // y => list of xs
+  type NumberMap = Map[Int, List[(Int, Int, Int)]] // y => List of value, x1, x2
 
   def main(args: Array[String]): Unit = {
     val content = Source.fromFile(args(0)).mkString
@@ -14,32 +13,36 @@ object GearRatio {
     val numberPattern = "\\d+".r
     val symbolPattern = "([^\\d\\.\\s])".r // not digit or dot or whitespace
 
-    val length = content.indexOf("\n") + 1
+    // length to help determine the actual coordinate
+    // because we're using one dimensional array (string)
+    val length = content.indexOf("\n") + 1 
 
-    given symbolMap: SymbolMap = symbolPattern
-      .findAllMatchIn(content)
+    // create symbol map to be used later when deciding number adjacency
+    val symbolMap = symbolPattern.findAllMatchIn(content)
       .foldLeft[SymbolMap](Map.empty)((acc, m) => {
         val key = m.start / length
         val newValue = acc.get(key) match
           case None        => List(m.start % length)
           case Some(value) => value :+ m.start % length
+
         acc.updated(key, newValue)
       })
 
-    val sum = numberPattern
-      .findAllMatchIn(content)
+    val sum = numberPattern.findAllMatchIn(content)
       .foldLeft[Int](0)((acc, m) => {
         val y = m.start / length
         val number = m.group(0).toInt
         val x1 = m.start % length
         val x2 = m.end % length - 1
-        if (adjacentSymbol(y, x1, x2)) (acc + number) else acc
+        if (adjacentSymbol(y, x1, x2)(symbolMap)) (acc + number) else acc
       })
 
     println(sum)
   }
 
-  def adjacentSymbol(y: Int, x1: Int, x2: Int)(using symbolMap: SymbolMap): Boolean =
+  // determine whether a number is adjacent to any symbols according
+  // to the symbol map
+  def adjacentSymbol(y: Int, x1: Int, x2: Int)(symbolMap: SymbolMap): Boolean =
     symbolMap.exists((sy, sxs) =>
       sy >= y - 1 && sy <= y + 1 && (sxs.exists(sx =>
         sx >= x1 - 1 && sx <= x2 + 1
