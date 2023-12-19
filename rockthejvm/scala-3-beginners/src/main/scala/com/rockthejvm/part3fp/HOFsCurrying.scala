4446b422418fa4efa81ac913589e8c691ede4e9b
Bustanil Arifin <bustanil@protonmail.com>
2023-12-19 09:23:49 +0800

    HOF and currying exercises

--- rockthejvm/scala-3-beginners/src/main/scala/com/rockthejvm/part3fp/HOFsCurrying.scala
+++ rockthejvm/scala-3-beginners/src/main/scala/com/rockthejvm/part3fp/HOFsCurrying.scala
@@ -32,11 +32,48 @@ object HOFsCurrying {
   val standardFormat: (Double => String) = curriedFormatter("%4.2f") // (x: Double) => "%4.2f".format(x)
   val preciseFormat: (Double => String) = curriedFormatter("%10.8f") // (x: Double) => "%4.2f".format(x)
 
+  /**
+   * 1. LList exercises
+   *    - foreach(A => Unit): Unit
+   *    - sort((A, A) => Int): LList[A]
+   *    - zipWith[B](LList[A], (A, A) => B): LList[B]
+   *    - foldLeft[B](start: B)((A, B) => B): B
+   * 2. toCurry(f: (Int, Int) => Int: Int => Int => Int
+   *    fromCurry(f: (Int => Int => Int): (Int, Int) => Int
+   *    compose(f, g) => x => f(g(x))
+   *    andThen(f, g) => x => g(f(x))
+   */
+
+  // 2
+  def toCurry[A, B, C](f: (A, B) => C): A => B => C = {
+    a => b => f(a, b)
+  }
+
+  def fromCurry[A, B, C](f: (A => B => C)): (A, B) => C = {
+    (a, b) => f(a)(b)
+  }
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C = x => f(g(x))
+  def andThen[A, B, C](f: A => B, g: B => C): A => C = x => g(f(x))
+
+
   def main(args: Array[String]): Unit = {
     println(nTimes(x => x + 1, 2, 2))
     println(nTimes_v2(x => x + 1, 2)(1000000))
     println(standardFormat(Math.PI))
     println(preciseFormat(Math.PI))
+
+    val add: (Int, Int) => Int = _ + _
+    val multiply: (Int, Int) => Int = _ * _
+    val curriedAdd = toCurry(add)
+    val curriedMultiply = toCurry(multiply)
+    val uncurriedAdd = fromCurry(curriedAdd)
+
+    println(curriedAdd(1)(2))
+    println(uncurriedAdd(1, 2))
+
+    println(compose(curriedAdd(1), curriedMultiply(3))(3))
+    println(andThen(curriedAdd(1), curriedMultiply(3))(3))
   }
 
 }
--- rockthejvm/scala-3-beginners/src/main/scala/com/rockthejvm/practice/LList.scala
+++ rockthejvm/scala-3-beginners/src/main/scala/com/rockthejvm/practice/LList.scala
@@ -1,5 +1,7 @@
 package com.rockthejvm.practice
 
+import scala.annotation.tailrec
+
 abstract class LList[T] {
 
   def head: T
@@ -9,6 +11,9 @@ abstract class LList[T] {
   def map[B](t: T => B): LList[B]
   def filter(p: T => Boolean): LList[T]
   def flatMap[B](t: T => LList[B]): LList[B]
+  def foreach(f: T => Unit): Unit
+  def zipWith[U](that: LList[T], f: (T, T) => U): LList[U]
+  def foldLeft[U](start: U)(f: (T, U) => U): U
   infix def ++(that: LList[T]): LList[T]
   override def toString: String = super.toString
 
@@ -22,7 +27,11 @@ case class Empty[T]() extends LList[T] {
   override def map[B](transformer: T => B): LList[B] = Empty()
   override def filter(p: T => Boolean): LList[T] = this
   override def flatMap[B](t: T => LList[B]): LList[B] = Empty()
+  override def foreach(f: T => Unit): Unit = ()
   override infix def ++(that: LList[T]): LList[T] = that
+
+  override def zipWith[U](that: LList[T], f: (T, T) => U): LList[U] = Empty[U]()
+  override def foldLeft[U](start: U)(f: (T, U) => U): U = start
 }
 
 // override val -> define accessors which is also a constructor argument (WOW!)
@@ -39,6 +48,20 @@ case class Cons[T](head: T, tail: LList[T]) extends LList[T] {
   override def flatMap[B](t: T => LList[B]): LList[B] = t.apply(head) ++ tail.flatMap(t)
 
   override infix def ++(that: LList[T]): LList[T] = Cons(head, tail ++ that)
+
+  override def foreach(f: T => Unit): Unit = {
+    f(head)
+    tail.foreach(f)
+  }
+
+  override def zipWith[U](that: LList[T], f: (T, T) => U): LList[U] = {
+    if (that.isEmpty) Empty[U]()
+    else Cons(f(head, that.head), tail.zipWith(that.tail, f))
+  }
+
+  override def foldLeft[U](start: U)(f: (T, U) => U): U = {
+    tail.foldLeft(f(head, start))(f)
+  }
 }
 
 trait Predicate[T] {
@@ -50,33 +73,42 @@ trait Transformer[A, B] {
 }
 
 object LList {
+  @tailrec
+  def find[A](list: LList[A], predicate: A => Boolean): A = {
+    if (list.isEmpty) throw new NoSuchElementException
+    else if (predicate.apply(list.head)) list.head
+    else find(list.tail, predicate)
+  }
+
   def main(args: Array[String]): Unit = {
     val test = Cons(1, Cons(2, Cons(3, Empty())))
-    val test2 = Empty().add(6).add(5).add(4)
-    println(test)
-    println(test2)
+    val test2 = Empty().add(6).add(5).add(4).add(3)
+//    println(test)
+//    println(test2)
+//
+//    val animals = Cons("cat", Cons("dog", Cons("horse", Empty())))
+//    println(animals)
+//
+//    val testMap = test.map[Int](a => a * 2)
+//
+//    println(testMap)
+//
+//    val testFilter = test.filter((value: Int)=> value < 2)
+//
+//    println(testFilter)
+//
+//    println(test ++ test2)
+//
+//
+//    val f = find(test, (value: Int) => value == 1)
+//
+//    println(f)
 
-    val animals = Cons("cat", Cons("dog", Cons("horse", Empty())))
-    println(animals)
+    test2.foreach(println)
+    test2.zipWith(test, _ + _).foreach(println)
 
-    val testMap = test.map[Int](a => a * 2)
-
-    println(testMap)
-
-    val testFilter = test.filter((value: Int)=> value < 2)
-
-    println(testFilter)
-
-    println(test ++ test2)
-
-    def find[A](list: LList[A], predicate: A => Boolean): A = {
-      if (list.isEmpty) throw new NoSuchElementException
-      else if (predicate.apply(list.head)) list.head
-      else find(list.tail, predicate)
-    }
-
-    val f = find(test, (value: Int) => value == 1)
-
-    println(f)
+    println(test2.foldLeft(0)(_ + _))
+    val add: (Int, Int) => Int = _ + _
+    println(Empty[Int]().foldLeft(0)(add))
   }
 }
