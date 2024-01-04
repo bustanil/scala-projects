6910b144682e1e67c2c368323753a0652e66da88
Bustanil Arifin <bustanil@protonmail.com>
2024-01-04 21:48:32 +0800

    lazy list

---
+++ rockthejvm/scala3-advanced/src/main/scala/com/rockthejvm/practice/LzListPlayground.scala
@@ -0,0 +1,103 @@
+package com.rockthejvm.practice
+
+abstract class LzList[A] {
+  def isEmpty: Boolean
+  def head: A
+  def tail: LzList[A]
+
+  // utilities
+  def #::(element: A): LzList[A] // prepending
+  def ++(another: LzList[A]): LzList[A] // TODO warning
+
+  // classics
+  def foreach(f: A => Unit): Unit
+  def map[B](f: A => B): LzList[B]
+  def flatMap[B](f: A => LzList[B]): LzList[B]
+  def filter(p: A => Boolean): LzList[A]
+  def withFilter(p: A => Boolean): LzList[A] = filter(p)
+
+  def take(n: Int): LzList[A]
+  def takeAsList(n: Int): List[A]
+  def toList: List[A] // use this carefully on large instance
+}
+
+case class LzEmpty[A]() extends LzList[A] {
+
+  override def isEmpty: Boolean = true
+
+  override def head: A = throw new NoSuchElementException
+
+  override def tail: LzList[A] = throw new NoSuchElementException
+
+  override def #::(element: A): LzList[A] = LzCons[A](element, this)
+
+  override def ++(another: LzList[A]): LzList[A] = another
+
+  override def foreach(f: A => Unit): Unit = ()
+
+  override def map[B](f: A => B): LzList[B] = LzEmpty[B]()
+
+  override def flatMap[B](f: A => LzList[B]): LzList[B] = LzEmpty[B]()
+
+  override def take(n: Int): LzList[A] =
+    if (n == 0) this
+    else throw new RuntimeException(s"Cannot take $n elements from an empty lazy list")
+
+  override def takeAsList(n: Int): List[A] = take(n).toList
+
+  override def toList: List[A] = List.empty
+
+  override def filter(p: A => Boolean): LzList[A] = this
+}
+
+class LzCons[A](hd: => A, tl: => LzList[A]) extends LzList[A] {
+
+  override lazy val head: A = hd
+  override lazy val tail: LzList[A] = tl
+
+  override def isEmpty: Boolean = false
+
+  override def #::(element: A): LzList[A] = LzCons(element, this)
+
+  override def ++(another: LzList[A]): LzList[A] = LzCons[A](head, tail ++ another)
+
+  override def foreach(f: A => Unit): Unit =
+    f(head)
+    tail.foreach(f)
+
+  override def map[B](f: A => B): LzList[B] = LzCons[B](f(head), tail.map(f))
+
+  override def flatMap[B](f: A => LzList[B]): LzList[B] = f(head) ++ tail.flatMap(f)
+  override def take(n: Int): LzList[A] = {
+    if (n < 0) LzEmpty[A]()
+    else LzCons[A](head, tail.take(n - 1))
+  }
+
+  override def takeAsList(n: Int): List[A] = {
+    if (n < 0) List.empty
+    else head +: tail.takeAsList(n - 1)
+  }
+
+  override def toList: List[A] = head +: tail.toList
+
+  override def filter(p: A => Boolean): LzList[A] =
+    if (p(head)) LzCons[A](head, tail.filter(p))
+    else tail.filter(p)
+}
+
+object LzList {
+  def generate[A](start: A)(generator: A => A): LzList[A] =
+    LzCons[A](start, LzList.generate(generator(start))(generator))
+
+  def from[A](list: List[A]): LzList[A] =
+    if (list.isEmpty) LzEmpty[A]()
+    else LzCons[A](list.head, LzList.from(list.tail))
+}
+
+object LzListPlayground {
+
+  def main(args: Array[String]): Unit = {
+    LzList.generate(1)(n => n + 1)
+  }
+
+}
